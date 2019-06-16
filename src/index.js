0e8d99a37a051b0a654d5d17ad1a07d591e1b664
v-boy <visheshsingh16@gmail.com>
2019-06-16 14:41:27 -0500

    dropdown

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import ToolTip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
 import './styles/tooltip.css';
+import './styles/dropdown.css';
 
+// create a tooltip
 const tooltip = new ToolTip(document.querySelector('.tooltip'));
 tooltip.init();
+
+// create a dropdown
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
