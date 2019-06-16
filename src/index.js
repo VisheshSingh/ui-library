078a897e3e1bc880217a619316b022e5e07357d0
v-boy <visheshsingh16@gmail.com>
2019-06-16 15:39:06 -0500

    Tabbed content

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import ToolTip from './ninja-ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/Tabs';
 import './styles/tooltip.css';
 import './styles/dropdown.css';
+import './styles/tabs.css';
 
 // create a tooltip
 const tooltip = new ToolTip(document.querySelector('.tooltip'));
@@ -13,3 +15,7 @@ dropdowns.forEach(dropdown => {
   const instance = new Dropdown(dropdown);
   instance.init();
 });
+
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
